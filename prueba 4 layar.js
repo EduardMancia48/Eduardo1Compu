2854dbd834c36d4260268b25150b357075028dca
EduardMancia48 <40743410+EduardMancia48@users.noreply.github.com>
2018-07-07 21:53:43 -0600

    Add files via upload

---
+++ prueba 4 layar.js
@@ -0,0 +1,62 @@
+// JavaScript Document{
+  "errorCode": 0,
+  "errorString": "ok", 
+  "hotspots": [
+    
+
+  	{
+      "anchor": {
+        "geolocation": {
+          "lat": 17.0798, 
+          "lon": -96.74528
+        }
+      }, 
+      "icon": {
+        "url": "https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Estadio_Tec.jpg"
+      }, 
+      "id": 1, 
+      "text": {
+        "description": "El INSTITUTO TÉCNICO EXSAL, es una Institución Educativa Laica Católica con inspiración salesiana no lucrativa y con responsabilidad social, fundada a finales de 1981 con la finalidad de multiplicar la obra educativa de San Juan Bosco. Atendiendo los niveles de Educación Parvularia, Básica y Media.", 
+        "title": "ITEXSAL"
+      },
+      "imageURL":"https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Estadio_Tec.jpg"
+    },
+
+    {
+      "anchor": {
+        "geolocation": {
+          "lat": 17.066934, 
+          "lon": -96.712344
+        }
+      }, 
+      "icon": {
+        "url": "https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Polideportivo_Equipo.jpg"
+      }, 
+      "id": 2, 
+      "text": {
+        "description": "El Polideportivo Venustiano Carranza es un espacio público que se pondrá a disposición de deportistas de alto rendimiento así como de las familias oaxaqueñas, en el que podrán desarrollar diversas disciplinas en las mejores condiciones de seguridad y confort.", 
+        "title": "Polideportivo Venustiano Carranza"
+      },
+      "imageURL":"https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Polideportivo_Equipo.jpg"
+    },
+
+    {
+      "anchor": {
+        "geolocation": {
+          "lat": 17.070267, 
+          "lon": -96.713577
+        }
+      }, 
+      "icon": {
+        "url": "https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Estadio_Beisbol_Equipo.jpg"
+      }, 
+      "id": 3, 
+      "text": {
+        "description": "El Estadio Eduardo Vasconcelos es la sede del equipo de béisbol Guerreros de Oaxaca que participa en la Liga Mexicana de Béisbol; Dicho estadio fue construido en 1950, y estuvo un tiempo administrado por la Universidad Autónoma Benito Juárez de Oaxaca.", 
+        "title": "Estadio Eduardo Vasconcelos"
+      },
+      "imageURL":"https://raw.githubusercontent.com/MATA9310/PixelSYM-Layar/master/Fotos/Estadio_Beisbol_Equipo.jpg"
+    }
+  ],
+  "layer": "Eduardo Mancia"
+} JavaScript Document
